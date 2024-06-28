2b21c9ac835fe8a8801a3a1dc2a2c3a0da9d1d58
Rohan <rohankumar5803@gmail.com>
2024-06-28 22:10:45 +0530

    adds city names

--- scripts/middle.js
+++ scripts/middle.js
@@ -33,3 +33,5 @@ const displayPopularRestaurants = ()=>{
     popularRestaurantsFlag = false;
   }
 }
+
+let citiesDelivered = "Delhi NCR Kolkata Mumbai Bengaluru Pune Hyderabad Chennai Lucknow Kochi Jaipur Ahmedabad Chandigarh Goa Indore Gangtok Nashik Ooty Shimla Ludhiana Guwahati Amritsar Kanpur Allahabad Aurangabad Bhopal Ranchi Visakhapatnam Bhubaneswar Coimbatore Mangalore Vadodara Nagpur Agra Dehradun Mysore Puducherry Surat Varanasi Patna Udaipur Srinagar Khajuraho Neemrana Cuttack Trivandrum Haridwar Leh Pushkar Rajkot Madurai Kozhikode Alappuzha Thrissur Manipal Vijayawada Jodhpur Kota Ajmer Mussoorie Rishikesh Jalandhar Jammu Manali All delivery cities"
