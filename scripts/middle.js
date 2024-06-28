f113c5f229b7ca843fe1124db1363bbd710ed488
Rohan <rohankumar5803@gmail.com>
2024-06-28 22:06:44 +0530

    adds js code for popular restaurants inner div

--- scripts/middle.js
+++ scripts/middle.js
@@ -1,5 +1,5 @@
 let popularCuisines =
-  "<div class='popular-cuisines'><div>American food near me</div>&#x2022;<div>Arabian food near me</div>&#x2022;<div>Bakery food near me</div>&#x2022;<div>Beverages food near me</div>&#x2022;<div>Biryani food near me</div>&#x2022;<div>Burger food near me</div>&#x2022;<div>Chinese food near me</div>&#x2022;<div>Desserts food near me</div>&#x2022;<div>Ice Cream food near me</div>&#x2022;<div>Juices food near me</div>&#x2022;<div>Kebab food near me</div>&#x2022;<div>Kerala food near me</div>&#x2022;<div>Mughlai food near me</div>&#x2022;<div>North Indian food near me</div>&#x2022;<div>Pizza food near me</div>&#x2022;<div>Salad food near me</div>&#x2022;<div>Sandwich food near me</div>&#x2022;<div>Shake food near me</div>&#x2022;<div>South Indian food near me</div>&#x2022;<div>Street food near me</div> </div>";
+  "<div class='explore-options-inside-div'><div>American food near me</div>&#x2022;<div>Arabian food near me</div>&#x2022;<div>Bakery food near me</div>&#x2022;<div>Beverages food near me</div>&#x2022;<div>Biryani food near me</div>&#x2022;<div>Burger food near me</div>&#x2022;<div>Chinese food near me</div>&#x2022;<div>Desserts food near me</div>&#x2022;<div>Ice Cream food near me</div>&#x2022;<div>Juices food near me</div>&#x2022;<div>Kebab food near me</div>&#x2022;<div>Kerala food near me</div>&#x2022;<div>Mughlai food near me</div>&#x2022;<div>North Indian food near me</div>&#x2022;<div>Pizza food near me</div>&#x2022;<div>Salad food near me</div>&#x2022;<div>Sandwich food near me</div>&#x2022;<div>Shake food near me</div>&#x2022;<div>South Indian food near me</div>&#x2022;<div>Street food near me</div> </div>";
 
 let popularCuisinesDiv = document.querySelector(".popularCuisinesDiv");
 
@@ -17,4 +17,19 @@ const displayPopularCuisines = () => {
 };
 
 let popularRestaurants =
-  "<div><div>Bakeries near me</div> <div>&#x2022;</div> <div>Beverage Shops near me</div> <div>&#x2022;</div> <div>Bhojanalya near me</div> <div>&#x2022;</div> <div>Casual Dining near me</div> <div>&#x2022;</div> <div>Dessert Parlors near me</div> <div>&#x2022;</div> <div>Food Courts near me</div> <div>&#x2022;</div> <div>Lounges near me</div> <div>&#x2022;</div> <div>Quick Bites near me</div></div>";
+  "<div class='explore-options-inside-div'><div>Bakeries near me</div> <div>&#x2022;</div> <div>Beverage Shops near me</div> <div>&#x2022;</div> <div>Bhojanalya near me</div> <div>&#x2022;</div> <div>Casual Dining near me</div> <div>&#x2022;</div> <div>Dessert Parlors near me</div> <div>&#x2022;</div> <div>Food Courts near me</div> <div>&#x2022;</div> <div>Lounges near me</div> <div>&#x2022;</div> <div>Quick Bites near me</div></div>";
+
+let popularRestaurantsDiv = document.querySelector(".popularRestaurantsDiv");
+
+let popularRestaurantsFlag = false;
+
+const displayPopularRestaurants = ()=>{
+  if (!popularRestaurantsFlag) {
+    popularRestaurantsDiv.innerHTML = popularRestaurants;
+    popularRestaurantsFlag = true;
+    arrow = document.querySelector(".arrow2").classList.add("right-arrow");
+  } else {
+    popularRestaurantsDiv.innerHTML = "";
+    popularRestaurantsFlag = false;
+  }
+}
